3708405ed6e8c1a3c0d1acf7dd3605335f221bc3
Iago V. R. <iagovieira3562@gmail.com>
2025-10-25 02:29:52 -0300

    feat: Adicionado a validacao na criacao de tags usando Service

--- app/src/main/java/br/com/unit/sistema/app/controller/TagsController.java
+++ app/src/main/java/br/com/unit/sistema/app/controller/TagsController.java
@@ -1,33 +1,48 @@
 package br.com.unit.sistema.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.unit.sistema.app.controller.dto.TagsDTO;
+import br.com.unit.sistema.app.controller.dto.CreateTagsDTO;
+import br.com.unit.sistema.app.entity.Tags;
+import br.com.unit.sistema.app.repository.TagsRepositorys;
+import br.com.unit.sistema.app.services.TagsService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/tags")
 public class TagsController {
 
+    @Autowired
+    TagsRepositorys tagsRepositorys;
+
+    @Autowired
+    TagsService tagsService;
+
     @PostMapping
-    public ResponseEntity<TagsDTO> createTag(@RequestBody TagsDTO tag) {
-        System.out.println(tag.toString());
+    public ResponseEntity<CreateTagsDTO> createTag(@RequestBody @Valid CreateTagsDTO data) {
+        tagsService.create(data);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/{idTag}")
-    public ResponseEntity<TagsDTO> getTag(@PathVariable long idTag) {
-        TagsDTO dummy = new TagsDTO("demo-tag-" + idTag, "#FFFFFF");
-        return new ResponseEntity<>(dummy, HttpStatus.OK);
+    @GetMapping
+    public Page<Tags> listarTags(@PageableDefault(size=10) Pageable paginacao) {
+        return tagsRepositorys.findAll(paginacao);
     }
+
+    // @GetMapping("/{idTag}")
+    // public ResponseEntity<TagsDTO> getTag(@PathVariable long idTag) {
+    //     return new ResponseEntity<>(dummy, HttpStatus.OK);
+    // }
 
 
 }
