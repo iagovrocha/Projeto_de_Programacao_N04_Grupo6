1bafaf37f5c5a8a6cf63f7ce9cafd663687b9fd2
Iago V. R. <iagovieira3562@gmail.com>
2025-10-28 16:10:07 -0300

    Comentado o codigo da Classe Evento devido a distancia padrao do resto do projeto envolvido

--- app/src/main/java/br/com/unit/sistema/app/entity/Evento.java
+++ app/src/main/java/br/com/unit/sistema/app/entity/Evento.java
@@ -1,35 +1,31 @@
 package br.com.unit.sistema.app.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
-import com.projeto.semana1_projetoprog.Usuarios.*;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 
-@Data
-@Entity
-@Table(name = "eventos")
+// @Data
+// @Entity
+// @Table(name = "eventos")
 
 public class Evento {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // private Long id;
 
-    private String nome;
-    private String descricao;
-    private String local;
-    private LocalDateTime dataHora;
-    private String status;
+    // private String nome;
+    // private String descricao;
+    // private String local;
+    // private LocalDateTime dataHora;
+    // private String status;
 
-    @ManyToOne
-    @JoinColumn(name = "organizador_id")
-    private Organizador organizador;
+    // @ManyToOne
+    // @JoinColumn(name = "organizador_id")
+    // private Organizador organizador;
 
-    @ManyToMany(mappedBy = "eventosInscritos")
-    private List<Participante> participantes = new ArrayList<>();
+    // @ManyToMany(mappedBy = "eventosInscritos")
+    // private List<Participante> participantes = new ArrayList<>();
 
 }
