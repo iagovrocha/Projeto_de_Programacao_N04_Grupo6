7e2cc82030887c3cc87e7e0eaa13038aabb8120e
João Paulo <joao.lordelo@souunit.com.br>
2025-10-28 22:24:39 -0300

    fix: Remoção do metodo no repositorio de Notificacao

--- app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoRepository.java
+++ app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.unit.sistema.app.entity.Notificacao;
 
 public interface NotificacaoRepository extends JpaRepository<Notificacao, Long>{
-
-    public boolean existsById(Long id);
     
 }
