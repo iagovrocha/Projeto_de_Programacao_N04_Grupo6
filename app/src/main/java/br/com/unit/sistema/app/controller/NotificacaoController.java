5c08e5d2d91c110677b8c7143ad8a80a020bbb21
João Paulo <joao.lordelo@souunit.com.br>
2025-10-28 22:10:33 -0300

    feat: Feature de exibir uma notificação especifica e exibir todas as notificacoes de um User especifico

--- app/src/main/java/br/com/unit/sistema/app/controller/NotificacaoController.java
+++ app/src/main/java/br/com/unit/sistema/app/controller/NotificacaoController.java
@@ -1,16 +1,21 @@
 package br.com.unit.sistema.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.unit.sistema.app.controller.dto.NotificacaoColetaDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoLidaDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoListagemDTO;
@@ -29,6 +34,18 @@ public class NotificacaoController {
     @Transactional(readOnly = true)
     public Page<NotificacaoListagemDTO> listarNotificacao(Pageable paginacao){
        return service.coletarNotificacao(paginacao);
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public NotificacaoListagemDTO mostrarNotificacaoEspecifica(@PathVariable long id){
+        return service.exibirNotificacaoEspecifica(id);
+    }
+
+    @GetMapping("/byUser")
+    @Transactional(readOnly = true)
+    public List<NotificacaoListagemDTO> listarNotificacaoUsuario(@RequestBody @Valid NotificacaoColetaDTO dados){
+       return service.coletarNotificacaoUsuario(dados);
     }
 
     @PostMapping
