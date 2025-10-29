117c448e6cc83cff8e1fe6d04659e2106a6acbfd
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-10-29 15:36:55 -0300

    feat: Criação do método deleteByIdNotificacao no repository Notificacao

--- app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoUsuarioRepository.java
+++ app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoUsuarioRepository.java
@@ -1,6 +1,10 @@
 package br.com.unit.sistema.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.unit.sistema.app.entity.NotificacaoUsuario;
 import br.com.unit.sistema.app.entity.NotificacaoUsuarioID;
@@ -8,4 +12,9 @@ import br.com.unit.sistema.app.entity.NotificacaoUsuarioID;
 public interface NotificacaoUsuarioRepository extends JpaRepository<NotificacaoUsuario,NotificacaoUsuarioID>{
     
     public boolean existsById(NotificacaoUsuarioID idNotificacaoUsuario);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM NotificacaoUsuario nu WHERE nu.id.idNotificacao = :idNotificacao")
+    public void deleteByIdNotificacao(@Param("idNotificacao") Long idNotificacao);
 }
