e92da07cba076ac88bc17b60be6daa5562012663
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-10-30 10:05:52 -0300

    feat: Adição dos metodos findByIdUser e findByTipo

--- app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoRepository.java
+++ app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoRepository.java
@@ -1,9 +1,15 @@
 package br.com.unit.sistema.app.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.unit.sistema.app.entity.Notificacao;
+import br.com.unit.sistema.app.entity.Tipo;
 
 public interface NotificacaoRepository extends JpaRepository<Notificacao, Long>{
     
+    public Page<Notificacao> findByIdUser(long idUser, Pageable paginacao);
+
+    public Page<Notificacao> findByTipo(Tipo tipo, Pageable paginacao);
 }
