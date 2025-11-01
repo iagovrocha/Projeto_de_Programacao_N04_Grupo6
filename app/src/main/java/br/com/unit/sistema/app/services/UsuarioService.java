cbcb2848b9058ea7f0dc3b7cd88c871a85a4a855
João Gabriel <jgmr0308@gmail.com>
2025-11-01 14:55:33 -0300

    Imports corrigidos, lista de entidades substituída por uma de responseDTO, geração dos objetos corrigida.

--- app/src/main/java/br/com/unit/sistema/app/services/UsuarioService.java
+++ app/src/main/java/br/com/unit/sistema/app/services/UsuarioService.java
@@ -1,9 +1,11 @@
 package br.com.unit.sistema.app.services;
 
-// import br.com.unit.sistema.app.controller.dto.CriarUsuarioDto;
-// import br.com.unit.sistema.app.entity.Usuario;
+import br.com.unit.sistema.app.controller.dto.UsuarioCreateDTO;
+import br.com.unit.sistema.app.controller.dto.UsuarioResponseDTO;
+import br.com.unit.sistema.app.entity.UsuarioEntidade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 import br.com.unit.sistema.app.repository.UsuarioRepository;
 
@@ -13,13 +15,23 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    // public void criarUsuario (CriarUsuarioDto criarUsuarioDto) {
-    //     var usuario = new Usuario(null, criarUsuarioDto.email(), criarUsuarioDto.senha());
+    public void criarUsuario (UsuarioCreateDTO usuarioCreateDTO) {
+        var usuario = new UsuarioEntidade();
+        usuario.setEmail(usuarioCreateDTO.getEmail());
+        usuario.setNome(usuarioCreateDTO.getNome());
+        usuario.setSenha(usuarioCreateDTO.getSenha());
 
-    //     usuarioRepository.save(usuario);
-    // }
+        usuarioRepository.save(usuario);
+    }
 
-    // public List<Usuario> listarUsuarios() {
-    //     return usuarioRepository.findAll();
-    // }
+     public List<UsuarioResponseDTO> listarUsuarios() {
+        return usuarioRepository.findAll()
+        .stream()
+        .map(usuario -> new UsuarioResponseDTO(
+            usuario.getId(),
+            usuario.getNome(),
+            usuario.getEmail()
+        ))
+        .toList();
+    }
 }
