18dc126174cdc2ef099235446b95fba02273a537
João Gabriel Mendonça Ribeiro <1241100281@acd.grupotiradentes.com>
2025-11-03 17:59:33 -0300

    Método de autenticação de usuário adicionado ao UsuarioService

--- app/src/main/java/br/com/unit/sistema/app/services/UsuarioService.java
+++ app/src/main/java/br/com/unit/sistema/app/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.com.unit.sistema.app.services;
 
+import br.com.unit.sistema.app.controller.dto.UsuarioAutenticacaoDTO;
 import br.com.unit.sistema.app.controller.dto.UsuarioCreateDTO;
 import br.com.unit.sistema.app.controller.dto.UsuarioResponseDTO;
 import br.com.unit.sistema.app.entity.UsuarioEntidade;
@@ -8,30 +9,46 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import br.com.unit.sistema.app.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class UsuarioService {
     
     @Autowired
     private UsuarioRepository usuarioRepository;
-
+    @Transactional
     public void criarUsuario (UsuarioCreateDTO usuarioCreateDTO) {
         var usuario = new UsuarioEntidade();
-        usuario.setEmail(usuarioCreateDTO.getEmail());
-        usuario.setNome(usuarioCreateDTO.getNome());
-        usuario.setSenha(usuarioCreateDTO.getSenha());
+        usuario.setEmail(usuarioCreateDTO.email());
+        usuario.setNome(usuarioCreateDTO.nome());
+        usuario.setSenha(usuarioCreateDTO.senha());
 
         usuarioRepository.save(usuario);
     }
-
-     public List<UsuarioResponseDTO> listarUsuarios() {
+    @Transactional
+    public List<UsuarioResponseDTO> listarUsuarios() {
         return usuarioRepository.findAll()
         .stream()
         .map(usuario -> new UsuarioResponseDTO(
             usuario.getId(),
             usuario.getNome(),
-            usuario.getEmail()
+            usuario.getEmail(),
+            usuario.getRole()
         ))
         .toList();
+    }
+    public UsuarioResponseDTO autenticarUsuario(UsuarioAutenticacaoDTO validarLogin){
+        UsuarioEntidade usuario = usuarioRepository.findAll()
+        .stream()
+        .filter(
+            u -> u.getEmail().equals(validarLogin.email()) && u.getSenha().equals(validarLogin.senha())
+        ).findFirst()
+        .orElse(null);
+        if (usuario == null){
+            return null;
+        }else{
+            UsuarioResponseDTO usuarioResposta = new UsuarioResponseDTO(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getRole());
+            return usuarioResposta;
+        }
     }
 }
