8d6ba73b3a6ca035e9ba19594e1de125d052ad74
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-03 12:35:01 -0300

    feat: adição da criação de logs e de notificacoes para pagamento

--- app/src/main/java/br/com/unit/sistema/app/services/NotificacaoService.java
+++ app/src/main/java/br/com/unit/sistema/app/services/NotificacaoService.java
@@ -1,7 +1,9 @@
 package br.com.unit.sistema.app.services;
 
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +15,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.unit.sistema.app.controller.dto.CriarLogNotificacaoDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoColetaDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoDeletarDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoLidaDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoListagemDTO;
+import br.com.unit.sistema.app.entity.LogsNotificacao;
+import br.com.unit.sistema.app.entity.MethodTypeLog;
 import br.com.unit.sistema.app.entity.Notificacao;
 import br.com.unit.sistema.app.entity.NotificacaoUsuario;
 import br.com.unit.sistema.app.entity.NotificacaoUsuarioID;
+import br.com.unit.sistema.app.entity.Pagamentos;
+import br.com.unit.sistema.app.entity.Tipo;
+import br.com.unit.sistema.app.repository.LogsNotificacaoRepository;
 import br.com.unit.sistema.app.repository.NotificacaoRepository;
 import br.com.unit.sistema.app.repository.NotificacaoUsuarioRepository;
 import jakarta.validation.Valid;
@@ -33,15 +41,39 @@ public class NotificacaoService{
 
     @Autowired
     private NotificacaoUsuarioRepository notificacaoUsuarioRepository;
+
+    @Autowired
+    private LogsNotificacaoRepository logsNotificacaoRepository;
     
+    public void gerarNotificacaoPagamento(Pagamentos dados,Tipo tipo){
+        salvarNotificacao(new NotificacaoDTO(
+        "Novo Pagamento Realizado", 
+        "Pagamento: "+dados.getIdPagamento()+
+        "\nValor: "+dados.getValor()+
+        "\nData"+dados.getDataPagamento(),  
+        tipo,
+        null , 
+        Arrays.asList(dados.getIdUsuario())));
+    }
+
+    public void gerarLogNotificacao(CriarLogNotificacaoDTO dados){
+
+        logsNotificacaoRepository.save(new LogsNotificacao(dados ));
+
+    }
     //GET
+    //exibe todas as notificações registradas
     @Transactional(readOnly = true)
     public Page<NotificacaoListagemDTO> coletarNotificacao(@PageableDefault Pageable paginacao){
+        //gerarLogNotificacao(new CriarLogNotificacaoDTO(null, null, MethodTypeLog.GET, "coletarNotificacao"));
+        
         return repository.findAll(paginacao).map(notificacao -> new  NotificacaoListagemDTO(notificacao));
     }
 
+    //exibe uma notificação específica
     @Transactional(readOnly = true)
     public NotificacaoListagemDTO exibirNotificacaoEspecifica(long id){
+        //gerarLogNotificacao(new CriarLogNotificacaoDTO(id, null, MethodTypeLog.GET, "exibirNotificacaoEspecifica"));
         NotificacaoListagemDTO notificacao = new NotificacaoListagemDTO(repository.getReferenceById(id));
 
         return notificacao;
@@ -50,33 +82,38 @@ public class NotificacaoService{
     //exibir todas as notificacoes daquele usuario
     @Transactional(readOnly = true)
     public Page<NotificacaoListagemDTO> coletarNotificacaoUsuario(NotificacaoColetaDTO dados, @PageableDefault Pageable paginacao){
+        
 
         return notificacaoUsuarioRepository.findAllByIdUser(dados.idUser(), paginacao).map(notificacao -> new  NotificacaoListagemDTO(notificacao));
     }
 
+    //coleta notificações que um usuário enviou
     @Transactional(readOnly = true)
     public Page<NotificacaoListagemDTO> coletarNotificacaoEnviadas(NotificacaoColetaDTO dados, @PageableDefault Pageable paginacao){
         return repository.findByIdUser(dados.idUser(), paginacao).map(notificacao -> new NotificacaoListagemDTO(notificacao));
     }
 
+    //filtra notificação pelo tipo
     @Transactional(readOnly = true)
     public Page<NotificacaoListagemDTO> filtrarNotificacaoTipo(NotificacaoColetaDTO dados, @PageableDefault Pageable paginacao){
         return repository.findByTipo(dados.tipo(), paginacao).map(notificacao -> new NotificacaoListagemDTO(notificacao));
     }
 
     //POST
+    //registra uma nova notificacao
     @Transactional
     public ResponseEntity<NotificacaoDTO> salvarNotificacao(@Valid NotificacaoDTO dados){
         Notificacao notificacao = new Notificacao(dados);
         repository.save(notificacao);
+        gerarLogNotificacao(new CriarLogNotificacaoDTO(notificacao.getIdNotificacao(),notificacao.getIdUser(),MethodTypeLog.POST,"salvarNotificacao"));
         for(long id : dados.destinatarios()){
             notificacaoUsuarioRepository.save(new NotificacaoUsuario(id,notificacao.getIdNotificacao()));
         }
-
         return ResponseEntity.status(HttpStatus.CREATED).body(dados);
     }
 
     //PUT
+    //marca uma notificacao como lida
     @Transactional
     public ResponseEntity<NotificacaoLidaDTO> atualizarNotificacao(@Valid NotificacaoLidaDTO dados){
         try{
@@ -86,6 +123,7 @@ public class NotificacaoService{
             }
             NotificacaoUsuario notificacao = notificacaoUsuarioRepository.getReferenceById(id);
             notificacao.marcarLida();
+            gerarLogNotificacao(new CriarLogNotificacaoDTO(notificacao.getIdNotificacaoUsuario().getIdNotificacao(),notificacao.getIdNotificacaoUsuario().getIdUser(), MethodTypeLog.PUT, "atualizarNotificacao"));
             return ResponseEntity.ok(dados);
         }catch (Exception e ){
             System.out.println(e.getMessage());
@@ -94,11 +132,12 @@ public class NotificacaoService{
     }
 
     //DELETE
+    //apaga uma notificacao especifica
     @Transactional
     public ResponseEntity<NotificacaoDeletarDTO> apagarNotificacao(NotificacaoDeletarDTO dados){
         notificacaoUsuarioRepository.deleteByIdNotificacao(dados.idNotificacao());
         repository.deleteById(dados.idNotificacao());
-
+        gerarLogNotificacao(new CriarLogNotificacaoDTO(dados.idNotificacao(), null, MethodTypeLog.DELETE, "apagarNotificacao"));
         return ResponseEntity.notFound().build();
         }
 
