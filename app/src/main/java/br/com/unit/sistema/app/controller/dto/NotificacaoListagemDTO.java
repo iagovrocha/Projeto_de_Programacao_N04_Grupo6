99ac8a090a1956042026bbd97691399b0e2a01ea
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-03 18:15:49 -0300

    feat: Adição de horário no DTO de listagem de Notificações

--- app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoListagemDTO.java
+++ app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoListagemDTO.java
@@ -1,10 +1,12 @@
 package br.com.unit.sistema.app.controller.dto;
 
+import java.time.LocalDateTime;
+
 import br.com.unit.sistema.app.entity.Notificacao;
 
-public record NotificacaoListagemDTO(long idNotificacao, String titulo, String mensagem, boolean statusEnvio, Long idRemetente) {
+public record NotificacaoListagemDTO(long idNotificacao, String titulo, String mensagem, boolean statusEnvio, Long idRemetente, LocalDateTime horario) {
 
     public NotificacaoListagemDTO(Notificacao notificacao) {
-        this(notificacao.getIdNotificacao(),notificacao.getTitulo(),notificacao.getMensagem(),notificacao.isStatusEnvio(),notificacao.getIdUser());
+        this(notificacao.getIdNotificacao(),notificacao.getTitulo(),notificacao.getMensagem(),notificacao.isStatusEnvio(),notificacao.getIdUser(), notificacao.getDataHorarioEnvio());
     } 
 }
