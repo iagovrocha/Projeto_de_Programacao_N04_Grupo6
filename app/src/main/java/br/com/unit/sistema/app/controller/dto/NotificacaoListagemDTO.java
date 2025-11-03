d17fad71d6877579993f43d389b15197e4ac5e77
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-03 12:30:59 -0300

    fix: Correção do tipo de idRemetente

--- app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoListagemDTO.java
+++ app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoListagemDTO.java
@@ -2,7 +2,7 @@ package br.com.unit.sistema.app.controller.dto;
 
 import br.com.unit.sistema.app.entity.Notificacao;
 
-public record NotificacaoListagemDTO(long idNotificacao, String titulo, String mensagem, boolean statusEnvio, long idRemetente) {
+public record NotificacaoListagemDTO(long idNotificacao, String titulo, String mensagem, boolean statusEnvio, Long idRemetente) {
 
     public NotificacaoListagemDTO(Notificacao notificacao) {
         this(notificacao.getIdNotificacao(),notificacao.getTitulo(),notificacao.getMensagem(),notificacao.isStatusEnvio(),notificacao.getIdUser());
