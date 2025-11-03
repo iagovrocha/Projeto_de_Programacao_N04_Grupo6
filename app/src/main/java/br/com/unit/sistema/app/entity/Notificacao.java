49136836d0b0eea64b9d9d509345db522f585cc6
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-03 18:17:15 -0300

    feat: Adição de horário na classe Notificacao

--- app/src/main/java/br/com/unit/sistema/app/entity/Notificacao.java
+++ app/src/main/java/br/com/unit/sistema/app/entity/Notificacao.java
@@ -1,8 +1,11 @@
 package br.com.unit.sistema.app.entity;
 
+import java.time.LocalDateTime;
+
 import org.springframework.lang.Nullable;
 
 import br.com.unit.sistema.app.controller.dto.NotificacaoDTO;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -33,6 +36,9 @@ public class Notificacao {
     private String mensagem;
     private boolean statusEnvio;
 
+    @Column(name = "data_horario_envio")
+    private LocalDateTime dataHorarioEnvio;
+
     @Enumerated(EnumType.STRING)
     private Tipo tipo;
    
@@ -42,6 +48,7 @@ public class Notificacao {
         this.mensagem = dados.mensagem();
         this.tipo = dados.tipo();
         this.statusEnvio = true;
+        this.dataHorarioEnvio = LocalDateTime.now();
     }
 
 }
