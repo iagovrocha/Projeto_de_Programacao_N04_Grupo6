3d4dd6c514d5d455660c60b4368659cda6cd8e9f
rafaelcallmon <rafacalmon27@gmail.com>
2025-11-03 13:44:29 -0300

    Atualização com a main

--- app/src/main/java/br/com/unit/sistema/app/repository/UsuarioRepositoryX.java
+++
@@ -1,9 +0,0 @@
-package br.com.unit.sistema.app.repository;
-
-import br.com.unit.sistema.app.entity.UsuarioX;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepositoryX extends JpaRepository<UsuarioX, Long> {
-}
