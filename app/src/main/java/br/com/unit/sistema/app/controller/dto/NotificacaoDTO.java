739c31a1d9dcc6af225970c0a5ec620d3586f98f
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-05 23:46:11 -0300

    feat: Nova feature de envio de email e validações concluidas

--- app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoDTO.java
+++ app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoDTO.java
@@ -1,10 +1,13 @@
 package br.com.unit.sistema.app.controller.dto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.lang.Nullable;
 
+import br.com.unit.sistema.app.entity.Evento;
+import br.com.unit.sistema.app.entity.Pagamentos;
 import br.com.unit.sistema.app.entity.Tipo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -22,9 +25,32 @@ public record NotificacaoDTO(
     Long idRemetente,
 
     @NotNull
-    List<Long> destinatarios
+    List<Long> destinatarios,
+
+    Long idTag
     
     ) {
 
+    public NotificacaoDTO(Pagamentos dados, Tipo tipo){
+        this("Novo Pagamento Realizado", 
+        "Pagamento: "+dados.getIdPagamento()+
+        "Valor: "+dados.getValor()+
+        "Data"+dados.getDataPagamento(), 
+        tipo,
+        null,
+        Arrays.asList(dados.getIdUsuario()),
+        null);
+        }
+    
+    public NotificacaoDTO(Evento dados, Tipo tipo){
+        this("Novo Evento registrado",
+        "Evento: "+dados.getNome() +
+        "Local: "+dados.getLocal() + 
+        "ID_Evento: "+dados.getId(), 
+        tipo,
+        null,
+        Arrays.asList(dados.getIdUser()),
+        null);
+    }
 
 } 
