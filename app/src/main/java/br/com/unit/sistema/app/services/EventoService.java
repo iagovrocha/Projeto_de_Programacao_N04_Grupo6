2fc41580efd6d0f32cc3e67a84424d04b96d407a
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-05 23:53:53 -0300

    feat: Adição do metodo para gerar a notificacao no Service de Evento

--- app/src/main/java/br/com/unit/sistema/app/services/EventoService.java
+++ app/src/main/java/br/com/unit/sistema/app/services/EventoService.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.unit.sistema.app.controller.dto.NotificacaoDTO;
 import br.com.unit.sistema.app.entity.Evento;
+import br.com.unit.sistema.app.entity.Tipo;
 import br.com.unit.sistema.app.repository.EventoRepository;
 
 
@@ -15,6 +17,8 @@ public class EventoService {
     
     @Autowired
     private EventoRepository repository;
+
+    @Autowired NotificacaoService notificacaoService;
 
     public List<Evento> listarTodos() {
         return repository.findAll();
@@ -25,7 +29,9 @@ public class EventoService {
     }
 
     public Evento salvar(Evento evento) {
-        return repository.save(evento);
+        Evento eventoSalvo = repository.save(evento);
+        notificacaoService.salvarNotificacao(new NotificacaoDTO(eventoSalvo, Tipo.AVISO));
+        return eventoSalvo;
     }
 
     public void deletar(Long id) {
