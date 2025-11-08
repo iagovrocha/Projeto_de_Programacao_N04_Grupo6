589ce82e90e438bcc7671b44efb2475e0142cbf1
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-08 01:42:00 -0300

    feat: ADição do metodo para retornar idNotificacao

--- app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoUsuarioRepository.java
+++ app/src/main/java/br/com/unit/sistema/app/repository/NotificacaoUsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.unit.sistema.app.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,6 +18,12 @@ public interface NotificacaoUsuarioRepository extends JpaRepository<NotificacaoU
 
     @Query("SELECT nu FROM NotificacaoUsuario nu WHERE nu.id.idUser = :idUser")
     public Page<NotificacaoUsuario> findAllByIdUser(@Param("idUser") Long idUser, Pageable paginacao);
+
+    @Query("SELECT nu FROM NotificacaoUsuario nu WHERE nu.id.idUser = :idUser AND nu.id.idNotificacao = :idNotificacao") 
+    public NotificacaoUsuario getReferencedByIdUN(@Param("idUser") Long idUser, @Param("idNotificacao") Long idNotificacao);
+
+    @Query("SELECT nu.id.idNotificacao FROM NotificacaoUsuario nu WHERE nu.lida = false AND nu.id.idUser = :idUser")
+    public ArrayList<Long> findNewByIdUser(@Param("idUser") Long idUser);
 
     @Transactional
     @Modifying
