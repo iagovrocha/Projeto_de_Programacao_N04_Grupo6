e93746b697e7cf059ef099e7e611baef7b9a78c3
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2025-11-08 01:40:42 -0300

    faet: Adição do Tipo na listagem geral da notificacao

--- app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoListagemDTO.java
+++ app/src/main/java/br/com/unit/sistema/app/controller/dto/NotificacaoListagemDTO.java
@@ -3,10 +3,11 @@ package br.com.unit.sistema.app.controller.dto;
 import java.time.LocalDateTime;
 
 import br.com.unit.sistema.app.entity.Notificacao;
+import br.com.unit.sistema.app.entity.Tipo;
 
-public record NotificacaoListagemDTO(long idNotificacao, String titulo, String mensagem, boolean statusEnvio, Long idRemetente, LocalDateTime horario) {
+public record NotificacaoListagemDTO(long idNotificacao, String titulo, String mensagem, boolean statusEnvio, Long idRemetente, LocalDateTime horario, Tipo tipo) {
 
     public NotificacaoListagemDTO(Notificacao notificacao) {
-        this(notificacao.getIdNotificacao(),notificacao.getTitulo(),notificacao.getMensagem(),notificacao.isStatusEnvio(),notificacao.getIdUser(), notificacao.getDataHorarioEnvio());
+        this(notificacao.getIdNotificacao(),notificacao.getTitulo(),notificacao.getMensagem(),notificacao.isStatusEnvio(),notificacao.getIdUser(), notificacao.getDataHorarioEnvio(), notificacao.getTipo());
     } 
 }
