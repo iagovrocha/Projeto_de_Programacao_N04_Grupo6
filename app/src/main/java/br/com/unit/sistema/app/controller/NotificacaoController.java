c31c0b12a5b61d4cc087f7ddb03e4869486cc907
Iago V. R. <iagovieira3562@gmail.com>
2025-11-10 04:48:36 -0300

    refactor: Adicionado o cors ao backend

--- app/src/main/java/br/com/unit/sistema/app/controller/NotificacaoController.java
+++ app/src/main/java/br/com/unit/sistema/app/controller/NotificacaoController.java
@@ -1,11 +1,10 @@
 package br.com.unit.sistema.app.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +13,18 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.unit.sistema.app.controller.dto.NotificacaoDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoDeletarDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoLidaDTO;
 import br.com.unit.sistema.app.controller.dto.NotificacaoListagemDTO;
-import br.com.unit.sistema.app.controller.dto.NotificacaoUsuarioListDTO;
 import br.com.unit.sistema.app.entity.Tipo;
 import br.com.unit.sistema.app.services.NotificacaoService;
 import jakarta.validation.Valid;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/notificacao")
 public class NotificacaoController {
 
